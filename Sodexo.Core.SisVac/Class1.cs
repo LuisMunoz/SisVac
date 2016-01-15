73be9133502aabf594f8de685a5a4341a79d148a
Luis <luisleninmc@gmail.com>
2016-01-15 02:38:56 -0500

    .

---
+++ Sodexo.Core.SisVac/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sodexo.Core.SisVac
+{
+    public class Class1
+    {
+    }
+}
---
+++ Sodexo.DAL.SisVac/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sodexo.DAL.SisVac
+{
+    public class Class1
+    {
+    }
+}
