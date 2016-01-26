9e980364f1109e7adddc27b53f89f314c70c5296
Luis <luisleninmc@gmail.com>
2016-01-26 11:51:13 -0500

    .

---
+++ Sodexo.BL.SisVac/AplicacionBL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sodexo.DAL.SisVac.Entity;
+using Sodexo.DAL.SisVac.Data;
+
+namespace Sodexo.BL.SisVac
+{
+    public class AplicacionBL
+    {
+        public AplicacionDA data = new AplicacionDA();
+        public List<Aplicacion> ListaAplicacion() 
+        {
+            return data.ListaAplicacion();
+        }
+    }
+}
